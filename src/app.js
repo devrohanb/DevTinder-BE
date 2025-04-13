8ae0559917252ed24c0b772e9117dee1731b1f60
rohan-coditas <rohan.bari@coditas.com>
2025-04-13 14:08:40 +0530

    Plyaing with API routs, query params and route params for dynamic routes

--- src/app.js
+++ src/app.js
@@ -3,25 +3,10 @@ const express = require("express");
 const app = express();
 
 // TODO: Make use of get()
-app.get("/users", (req, res) => {
+app.get("/user/:userId/:empId", (req, res) => {
+  console.log(req.query);
+  console.log(req.params);
   res.send({ firstName: "Rohan", lastName: "Bari" }); // Update later
-});
-
-app.post("/users", (req, res) => {
-  // Add DB logic here
-  res.send("Users data saved to DB successfully !");
-});
-
-app.delete("/users", (req, res) => {
-  res.send("User deleted successfully ");
-});
-
-app.put("/users", (req, res) => {
-  res.send("User updated successfully");
-});
-
-app.use("/test", (req, res) => {
-  res.send("This is a test API endpoint");
 });
 
 app.listen(3000, () => {

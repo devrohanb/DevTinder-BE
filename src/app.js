7ba6b7a36665e5e91f67d66865605e06a0a6b6f3
rohan-coditas <rohan.bari@coditas.com>
2025-04-13 17:40:18 +0530

    Played with middlewares, next() and multiple response handlers

--- src/app.js
+++ src/app.js
@@ -2,12 +2,29 @@ const express = require("express");
 
 const app = express();
 
-// TODO: Make use of get()
-app.get("/user/:userId/:empId", (req, res) => {
-  console.log(req.query);
-  console.log(req.params);
-  res.send({ firstName: "Rohan", lastName: "Bari" }); // Update later
-});
+// app.use(
+//   "/users",
+//   (req, res, next) => {
+//     console.log("1st response log!");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("2nds response log !");
+//     res.send("send 2");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("3rd response log !");
+//     next();
+//     res.send("send 3");
+//   }
+// );
+
+// app.get("/user/:userId/:empId", (req, res) => {
+//   console.log(req.query);
+//   console.log(req.params);
+//   res.send({ firstName: "Rohan", lastName: "Bari" }); // Update later
+// });
 
 app.listen(3000, () => {
   console.log("Server started on PORT: 3000");
